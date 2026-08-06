826d4b895a9c0412e4aa45d17df4f4430e7c8faf
pluggeduser <pluggeduser@users.noreply.github.com>
2026-08-06 08:52:08 +0000

    deploy: f46ef922eebc623c5193f4c28824e7d5ebd3d622

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,8 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+
 gtag('js', new Date());
 gtag('config', 'G-7MLLVYGCEY');
